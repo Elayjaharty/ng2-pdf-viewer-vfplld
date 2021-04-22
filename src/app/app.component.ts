dee9c76d707a05d0c6ae7a31d4c1624cb57a4607
Elayjaharty <elayjaharty@gmail.com>
2021-04-22 23:07:14 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  pdfsrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+}
